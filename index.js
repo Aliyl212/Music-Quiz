439ace7fb429702fb7bd8d7438a3eaaa7173d7dc
Apple <apple@Apples-MacBook-Pro-97.local>
2019-06-14 11:08:18 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(answerOne);
+
+// AUDIO
+
+function celloSound(){
+    var cellosound = document.getElementById("myCello");
+    cellosound.play();
+}
+
+
+function saxSound(){
+    var saxsound = document.getElementById("mySax");
+    saxsound.play();
+}
+
+
+
+
+function clarinetSound(){
+    var clarinetsound = document.getElementById("myClarinet");
+    clarinetsound.play();
+}
+
+
+
+function violinSound(){
+    var violinsound = document.getElementById("myViolin");
+    violinsound.play();
+}
